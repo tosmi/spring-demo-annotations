4dc20862f5d49756e5cb5ecbdf52d4eef3b0afa2
Toni Schmidbauer <toni@stderr.at>
2020-10-25 21:43:03 +0100

    initial import

---
+++ src/at/stderr/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,11 @@
+package at.stderr.springdemo;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
---
+++ src/at/stderr/springdemo/Coach.java
@@ -0,0 +1,6 @@
+package at.stderr.springdemo;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+}
