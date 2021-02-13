95419f8b97130a616369f51d5896500bad58b8c1
Toni Schmidbauer <toni@stderr.at>
2021-02-13 20:19:32 +0100

    update

---
+++ src/at/stderr/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package at.stderr.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("at.stderr.springdemo")
+public class SportConfig {
+
+}
